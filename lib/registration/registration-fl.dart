d82ff81026278929b1d31f219913eef6b69051df
Jester <justjestervz@gmail.com>
2022-02-03 11:49:19 +0300

    work

--- lib/registration/registration-fl.dart
+++ lib/registration/registration-fl.dart
@@ -21,56 +21,236 @@ class _RegFL extends State<RegFL> {
   @override
   Widget build(BuildContext context) {
     return
-      Scaffold(
-          appBar: AppBar(),
-          resizeToAvoidBottomInset: false,
-          body: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child:  Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+        Scrollbar(
+          controller: _controller,
+          isAlwaysShown: true,
+            child: Scaffold(
+            appBar: AppBar(),
+            resizeToAvoidBottomInset: false,
+            body: Stack(
+              fit: StackFit.expand,
               children: [
-                Container(
-                  height: 1200,
+                Positioned(
+                    width: 375,
+                    left: 26,
+                    top: 26,
+                    child: Text(regTitle,
+                        style: const TextStyle(
+                            fontFamily: 'Bubicon-Bold', fontSize: 36.0))),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          family,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          patronymic,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          inn,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snils,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          contacts,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          email,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          lkPasswordText,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          repeatPassword,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          repeatPassword,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          repeatPassword,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          repeatPassword,
+                          style: TextStyle(color: colorGrayText, fontSize: 16.0),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(color: inputBorder))),
+                        )
+                      ],
+                    ),
+
+                  ],
+                ),
+                /*
+                Positioned(
+                  width: MediaQuery.of(context).size.width,
+                  top: 320,
                   child: Column(
                     children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            family,
-                            style: TextStyle(color: colorGrayText, fontSize: 16.0),
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide(color: inputBorder))),
-                          )
-                        ],
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(0, 0, 0, 18),
+                        child: SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            height: 55.0,
+                            child: ElevatedButton(
+                                onPressed: () {},
+                                child: Text(
+                                  clearPassword,
+                                  style: const TextStyle(fontSize: 18.0),
+                                ),
+                                style:
+                                ElevatedButton.styleFrom(primary: colorMain))),
                       ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            name,
-                            style: TextStyle(color: colorGrayText, fontSize: 16.0),
-                          ),
-                          TextField(
-                            decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                    borderSide: BorderSide(color: inputBorder))),
-                          )
-                        ],
-                      ),
-                    ],
-                  )
-                ),
 
+                      //ElevatedButton(onPressed: (){}, child: Text(login)),
+                    ],
+                  ),
+                ),
+                 */
               ],
-            )
-          )
-);
+            )));
   }
 }
