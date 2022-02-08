fbd17df6bec37afc284862170c866de0a94b55d9
Vladimir <justjestervz@gmail.com>
2022-02-08 18:52:01 +0500

    add contracts folder

---
+++ lib/contracts/contracts.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Contracts extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _Contracts();
+}
+
+class _Contracts extends State<Contracts>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+      bottomNavigationBar: ,
+    )
+  }
+}
