87cf6dc7627b8391bf6677e5252681ab321cc5a0
Vladimir <justjestervz@gmail.com>
2022-02-15 20:27:38 +0500

    add masks file

---
+++ lib/elements/masks.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class MaskInput extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _MaskInput();
+}
+
+class _MaskInput extends State<MaskInput>{
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
