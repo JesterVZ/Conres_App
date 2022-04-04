53b8d5ae961632973e884194dec9dd608f1643a7
Vladimir <justjestervz@gmail.com>
2022-04-04 13:17:17 +0500

    validation

--- lib/validation/validation.dart
+++ lib/validation/validation.dart
@@ -1,6 +1,7 @@
-class Validation{
-  bool isEmailvalidate(String email){
-    return RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$').hasMatch(email);
-  }
+bool isEmailValidate(String email){
+  return RegExp(r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$').hasMatch(email);
+}
 
+bool isPhoneValidate(String phone){
+  return RegExp(r'[0-9\-\+]{9,15}$').hasMatch(phone);
 }
