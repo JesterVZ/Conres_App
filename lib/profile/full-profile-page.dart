cc943bb839950a8885793bc8c2fa76c59138885b
Vladimir <justjestervz@gmail.com>
2022-05-26 19:42:48 +0500

    234234

--- lib/profile/full-profile-page.dart
+++ lib/profile/full-profile-page.dart
@@ -2,6 +2,7 @@ import 'package:conres_app/bloc/profile/profile-bloc.dart';
 import 'package:conres_app/bloc/profile/profile-state.dart';
 import 'package:conres_app/consts.dart';
 import 'package:conres_app/elements/header/header-notification.dart';
+import 'package:conres_app/profile/tabs/info-tab.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -10,134 +11,84 @@ import '../DI/dependency-provider.dart';
 import '../bloc/auth/auth-block.dart';
 import '../elements/bloc/bloc-screen.dart';
 
-class FullProfile extends StatefulWidget{
+class FullProfile extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _FullProfile();
 }
 
-class _FullProfile extends State<FullProfile>{
+class _FullProfile extends State<FullProfile>
+    with SingleTickerProviderStateMixin {
+  TabController? tabController;
+  List<Tab> tabs = [
+    const Tab(
+      icon: Icon(Icons.home),
+      text: "Общие",
+    ),
+    const Tab(
+      icon: Icon(Icons.phone),
+      text: "Контакты",
+    ),
+    const Tab(
+      icon: Icon(Icons.lock),
+      text: "Дов. лицо",
+    )
+  ];
+  List<Widget> panels = [
+    InfoTab(),
+    Center(child: Text("2")),
+    Center(child: Text("4")),
+  ];
   AuthBloc? authBloc;
   ProfileBloc? profileBloc;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocScreen<ProfileBloc, ProfileState>(
-      bloc: profileBloc,
-      listener: _listener,
-      builder: (context, state){
-        return Scaffold(
-          body: Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(bottom: 14),
-                child: HeaderNotification(text: "Основная информация"),
-              ),
-              Row(
-                children: [
-                  GestureDetector(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: 106,
-                      height: 77,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: colorMain
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
+        bloc: profileBloc,
+        listener: _listener,
+        builder: (context, state) {
+          return DefaultTabController(
+              length: 3,
+              child: Scaffold(
+                backgroundColor: defaultBackground,
+                  body: Column(
                         children: [
-                        Container(
-                          width: 32,
-                          height: 32,
-                          margin: EdgeInsets.only(bottom: 7),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white.withOpacity(0.1)
+                          Container(
+                            padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+                            margin: const EdgeInsets.only(bottom: 14),
+                            child:
+                                HeaderNotification(text: "Основная информация"),
+                            ),
+                          Container(
+                            padding: EdgeInsets.all(20),
+                            child: TabBar(
+                                    controller: tabController,
+                                    tabs: tabs,
+                                    unselectedLabelColor: Colors.black,
+                                    labelColor: Colors.white,
+                                    indicator: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        color: colorMain),
+                                  ),
                           ),
-                          child: SvgPicture.asset('assets/main.svg', color: Colors.white),
-                        ),
-                        Text("Общие", style: TextStyle(
-                          color: Colors.white
-                        ))
-                      ]),
-                    ),
-                  ),
-                  const Spacer(),
-                  GestureDetector(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: 106,
-                      height: 77,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: colorMain
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                        Container(
-                          width: 32,
-                          height: 32,
-                          margin: EdgeInsets.only(bottom: 7),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white.withOpacity(0.1)
-                          ),
-                          child: SvgPicture.asset('assets/contacts.svg', color: Colors.white,),
-                        ),
-                        Text("Контакты", style: TextStyle(
-                          color: Colors.white
-                        ))
-                      ]),
-                    ),
-                  ),
-                  const Spacer(),
-                  GestureDetector(
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: 106,
-                      height: 77,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: colorMain
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                        Container(
-                          width: 32,
-                          height: 32,
-                          margin: EdgeInsets.only(bottom: 7),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white.withOpacity(0.1)
-                          ),
-                          child: SvgPicture.asset('assets/confidant.svg', color: Colors.white),
-                        ),
-                        Text("Дов. лицо", style: TextStyle(
-                          color: Colors.white
-                        ))
-                      ]),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          )
-        );
-      });
+                          
+                          Expanded(
+                            child: TabBarView(
+                                controller: tabController,
+                                children: panels),
+                          )
+                        ],
+                      )));
+        });
   }
-  _listener(BuildContext context, ProfileState state) {
 
-  }
+  _listener(BuildContext context, ProfileState state) {}
 
   @override
   void didChangeDependencies() {
