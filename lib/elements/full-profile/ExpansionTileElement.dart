2651833ef117714984b260fc0c6274bee82b7554
Jester <justjestervz@gmail.com>
2022-05-30 11:41:40 +0300

    try to fix state bug

---
+++ lib/elements/full-profile/ExpansionTileElement.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ExpansionTileElement extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ExpansionTileElement();
+}
+
+class _ExpansionTileElement extends State<ExpansionTileElement> {
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile();
+  }
+}
