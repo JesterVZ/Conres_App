2646dd5d5fe2bbcf567645eea09f8c6b6ed3db75
Vladimir <justjestervz@gmail.com>
2022-08-09 19:19:40 +0500

    new file send dialog

--- lib/elements/chat/file-send-dialog.dart
+++ lib/elements/chat/file-send-dialog.dart
@@ -1,3 +1,4 @@
+import 'package:conres_app/consts.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class _FileSendDialog extends State<FileSendDialog>{
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text("Загрузка файлов"),
+      title: const Text("Загрузка файлов", textAlign: TextAlign.center,),
       content: GestureDetector(
         onTap: (){
           FocusScope.of(context).requestFocus(FocusNode());
@@ -21,19 +22,29 @@ class _FileSendDialog extends State<FileSendDialog>{
         child: Container(
           height: 100,
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8)
+            borderRadius: BorderRadius.circular(10)
           ),
           child: Column(
             children: [
               ElevatedButton(
                 onPressed: (){
                   widget.createPhoto.call();
-                }, 
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                                          backgroundColor: colorMain,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius: buttonRadius)),
                 child: const Text("Сделать фото")),
                 ElevatedButton(
                 onPressed: (){
                   widget.pickFile.call();
+                  Navigator.pop(context);
                 }, 
+                style: ElevatedButton.styleFrom(
+                                          backgroundColor: colorMain,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius: buttonRadius)),
                 child: const Text("Взять из галереи"))
             ],
           )
