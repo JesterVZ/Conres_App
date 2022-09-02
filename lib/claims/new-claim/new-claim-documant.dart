a5a4d1e4fca70453347bc0df8bca0533d6e98c00
Jester <justjestervz@gmail.com>
2022-09-02 11:48:15 +0300

    new claim doc

---
+++ lib/claims/new-claim/new-claim-documant.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../../consts.dart';
+
+class NewClaimDicument extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _NewClaimDicument();
+}
+
+class _NewClaimDicument extends State<NewClaimDicument> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(
+                defaultSidePadding, 0, defaultSidePadding, 0),
+            child: Column(children: [])));
+  }
+}
