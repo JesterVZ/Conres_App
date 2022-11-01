5a3c93494e87b2a51fd37cf28326b9f6028494cb
Jester <justjestervz@gmail.com>
2022-11-01 17:49:09 +0300

    service is ready

--- lib/claims/new-claim/new-claim-documant.dart
+++ lib/claims/new-claim/new-claim-documant.dart
@@ -1,3 +1,4 @@
+import 'package:conres_app/UI/main-form.dart';
 import 'package:conres_app/claims/new-claim/new-claim-step-1.dart';
 import 'package:conres_app/elements/header/header-notification.dart';
 import 'package:flutter/material.dart';
@@ -13,40 +14,45 @@ class NewClaimDicument extends StatefulWidget {
 }
 
 class _NewClaimDicument extends State<NewClaimDicument> {
+  Future<void> _refrash() async {}
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MainForm(
+        header: HeaderNotification(text: "Тип заявления", canGoBack: true),
         body: SingleChildScrollView(
-      child: Column(children: [
-        Padding(
-            padding: EdgeInsets.fromLTRB(
-                defaultSidePadding, 52, defaultSidePadding, 0),
-            child: Column(children: [
-              Container(
-                child: HeaderNotification(text: "Заявления"),
-              ),
-              ClaimTypeELement(
-                  text:
-                      "Заявление на технологическое присоединение к электрическим сетям.",
-                  page: NewClaimStep7()),
-              ClaimTypeELement(
-                  text:
-                      "Заявление о необходимости снятия показаний существующего прибора учета.",
-                  page: BaseClaimStep1()),
-              ClaimTypeELement(
-                  text:
-                      "Заявление на осуществление допуска в эксплуатацию прибора учета.",
-                  page: BaseClaimStep1()),
-              ClaimTypeELement(
-                  text:
-                      "Заявление на оборудование точки поставки приборами учета.",
-                  page: BaseClaimStep1()),
-              ClaimTypeELement(
-                  text:
-                      "Заявление на установку, замену и (или) эксплуатацию приборов учета.",
-                  page: BaseClaimStep1()),
-            ]))
-      ]),
-    ));
+            child: Padding(
+                padding: EdgeInsets.only(
+                    left: defaultSidePadding, right: defaultSidePadding),
+                child: Column(
+                  children: [
+                    ClaimTypeELement(
+                        text:
+                            "Заявление на технологическое присоединение к электрическим сетям.",
+                        page: NewClaimStep7()),
+                    ClaimTypeELement(
+                        text:
+                            "Заявление о необходимости снятия показаний существующего прибора учета.",
+                        page: BaseClaimStep1()),
+                    ClaimTypeELement(
+                        text:
+                            "Заявление на осуществление допуска в эксплуатацию прибора учета.",
+                        page: BaseClaimStep1()),
+                    ClaimTypeELement(
+                        text:
+                            "Заявление на оборудование точки поставки приборами учета.",
+                        page: BaseClaimStep1()),
+                    ClaimTypeELement(
+                        text:
+                            "Заявление на установку, замену и (или) эксплуатацию приборов учета.",
+                        page: BaseClaimStep1()),
+                  ],
+                ))),
+        onRefrash: _refrash);
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
   }
 }
